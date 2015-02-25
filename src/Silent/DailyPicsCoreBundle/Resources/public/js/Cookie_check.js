793f1a3456d331c64cc80ff5a552a43ca11a9ac8
SilentKernel <ludoz54@gmail.com>
2015-02-25 01:22:17 +0100

    Cookie notification base
    Added javascript for cookie notification

---
+++ src/Silent/DailyPicsCoreBundle/Resources/public/js/Cookie_check.js
@@ -0,0 +1,44 @@
+// This file generate the "cookie notification" for European Law
+
+// This is the string used with LocalStorage (HTML5)
+// We use LocalStorage instead of a simple cookie because the server does not need
+// this information (cookies are sent at every request to the server, not localStorage, save bandwith)
+const LSCString = "cns";
+
+function acceptCookie()
+{
+    // will put a string in localStorage to prevent showing the notification again
+    localStorage.setItem(LSCString, 1);
+    $("#cookie_notification").html("");
+}
+
+function declineCookie()
+{
+    // redirect client on an other website (will be customisable when it will be done)
+    window.location.replace("https://google.com");
+}
+
+// This function will get parameters later
+// to allow people to change the message from parameters.yml,
+// we will also allow webmaster to add a "more info" link
+function showCookieNotification()
+{
+    // Show only if client did not accepted Cookies (with local storage.
+    if (localStorage.getItem(LSCString) != 1)
+    {
+        $( "#main_container" ).prepend
+        (
+            '<div id = "cookie_notification" class = "col-xs-12 col-sm-12 col-md-12 col-lg-12"> ' +
+                '<div class = "alert alert-dismissible alert-info text-center">' +
+                    "Ce site utilise les cookies pour fonctionner, acceptez vous l'utilisation des cookies ?" +
+                    "<br />" +
+                    "<a class = 'btn btn-success' href='javascript:void(0);' onclick='acceptCookie()'>J'accepte</a> " +
+                    "<a class = 'btn btn-danger' href='javascript:void(0);' onclick='declineCookie()'>Je refuse</a>" +
+            '</div>' +
+            '</div>'
+        );
+    }
+}
+
+// to be removed soon
+showCookieNotification();
